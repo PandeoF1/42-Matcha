634f071939b7ec0c04fa3a2a9d53a4c03027bdf5
iot <iot@iot.42>
2024-01-11 01:06:03 +0100

    v2 without poll

--- server/database/database.py
+++ server/database/database.py
@@ -6,13 +6,12 @@ import dotenv
 dotenv.load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-DB = None
-
 async def get_database():
-    global DB
-    if DB is None:
-        db_pool = await asyncpg.create_pool(DATABASE_URL)
-        DB = db_pool
-        async with db_pool.acquire() as conn:
-            await conn.execute("""DELETE FROM token WHERE last_activity < $1""", datetime.datetime.now().timestamp() - 3600 * 6)
-    return DB
+    connexion = await asyncpg.connect(DATABASE_URL)
+    try:
+        yield connexion
+    except:
+        print("Error")
+    finally:
+        if connexion:
+            await connexion.close()
