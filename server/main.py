7355895c04f5b7a6c3e740459eadc03e50968f5d
Theo nard <tnard@z1r1p1.42lyon.fr>
2024-02-21 14:47:33 +0100

    fix: docker images

--- server/main.py
+++ server/main.py
@@ -43,4 +43,4 @@ app.include_router(status_controller)
 app.include_router(tags_controller)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8765, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8765, reload=True)
